4e6144e716d7672116a5840dc99c92b6009dd00d
harsh lodhi <harsh.lodhi.19e@iitram.ac.in>
2023-04-14 21:20:53 +0530

    commit

--- main.js
+++ main.js
@@ -5,6 +5,27 @@ window.addEventListener("scroll" , () => {
 
 const plusButton = document.getElementById("icon-faq-plus")
 
-docgetElementByClass
+
+const questions = document.querySelectorAll(".faq-small-container")
+
+questions.forEach(function(question){
+    const plusBtn = question.querySelector(".icon-faq-plus")
+    const faqQuestion = question.querySelector('.faq-question');
+    const minusBtn = question.querySelector(".icon-faq-minus")
+    plusBtn.addEventListener("click", function(){
+        faqQuestion.classList.toggle('open');
+        minusBtn.classList.toggle('open')
+        plusBtn.classList.toggle('open')
+    })
+
+    minusBtn.addEventListener("click",function(){
+        faqQuestion.classList.remove('open');
+        minusBtn.classList.toggle('open')
+        plusBtn.classList.toggle('open')
+    })
+
+
+})
+
 
 

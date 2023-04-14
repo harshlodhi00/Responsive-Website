d69de8ca0a5ae1294162dcba725d8a23b67cac23
harsh lodhi <harsh.lodhi.19e@iitram.ac.in>
2023-04-14 19:33:08 +0530

    commit

---
+++ main.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll" , () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 80 )
+})
+
+
+const plusButton = document.getElementById("icon-faq-plus")
+
+docgetElementByClass
+
+
